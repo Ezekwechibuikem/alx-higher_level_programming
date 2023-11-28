55568fcb9a85ff35a32fe7c7e9a9ebefaad00ea2
chibuikem ezekwe <ezekwechibuikem@gmail.com>
2023-11-27 18:46:27 -0800

    advanced task

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,13 +1,22 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- *
- *
- */
+  *check_cycle - exixtence of a cycle in a linked list.
+  *@list: parsed as arguments.
+  *Return: 1 if there is a cycle.
+  */
+
 int check_cycle(listint_t *list)
 {
-	listint_t *fast = lsit, *slow = lists;
-	
-	while(fast != NULL && slow != NULL && fast->next != NULL)
-	[
-		slow = slow->next
+	listint_t *ahead = list, *behind = list;
+
+	while (behind != NULL && ahead != NULL && ahead->next != NULL)
+	{
+		behind = behind->next;
+		ahead = ahead->next->next;
+
+		if (ahead == behind)
+			return (1);
+	}
+	return (0);
+}
