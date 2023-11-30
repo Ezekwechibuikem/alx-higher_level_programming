a3c17b37d44b5d9057e66549e5f5866dcce5d250
chibuikem ezekwe <ezekwechibuikem@gmail.com>
2023-11-30 10:57:53 -0800

    correction

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,10 +2,6 @@
 
 from add_0 import add
 
-def print_add_result(a, b):
-    print("{} + {} = {}".format(a, b, add(a, b)))
-
-if __name__ == "__main__":
-    a = 1
-    b = 2
-    print_add_result(a, b)
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add(a, b)))
