a6b9d58a30eddfae67af3af12cbcf6bc25c55dbe
chibuikem ezekwe <ezekwechibuikem@gmail.com>
2023-11-30 10:47:50 -0800

    task 1

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from add_0 import add
+
+def print_add_result(a, b):
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print_add_result(a, b)
