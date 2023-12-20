a77370806ff89a4ba7cae3519e8985a80302ed1c
Ezekwe chibuikem <nwachinaemelum@yahoo.com>
2023-12-20 05:14:55 -0800

    task 0 and 1

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Definition of class function Square"""
+
+
+class Square:
+    """empty Square class"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square class definition"""
+
+
+class Square:
+    """Square class."""
+
+    def __init__(self, size):
+        self._size = size
