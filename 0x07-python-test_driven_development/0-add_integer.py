455d56c4929f97d543935cd77e73f83a4d79aaaa
Ezekwe chibuikem <nwachinaemelum@yahoo.com>
2024-01-10 06:55:00 -0800

    name correction of task zero

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,14 +3,17 @@
 This module contains a function that adds 2 integers.
 """
 
-def add_interger(a, b=98):
+
+def add_integer(a, b=98):
     """
     This function adds 2 integers.
     """
     if type(a) is float:
         a = int(a)
-    if type(b) is not int:
+    if type(b) is float:
+        b = int(b)
+    if type(a) is not int:
         raise TypeError("a must be an integer")
     if type(b) is not int:
-        raise TypeError("b must be an interger")
+        raise TypeError("b must be an integer")
     return a + b
